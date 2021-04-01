23cd574404ba307aabd7092411f35aab7dd83d93
myusername <s3818475@rmit.edu.vn>
2021-04-01 19:45:25 +0700

    Second commit

--- src/main/StudentEnrolment/Course.java
+++ src/main/StudentEnrolment/Course.java
@@ -1,4 +1,49 @@
 package StudentEnrolment;
 
+import java.util.ArrayList;
+
 public class Course {
+    private String cName;
+    private String cID;
+    private int credits;
+    private ArrayList<Student> sList;
+
+    public Course() {
+        this.cName = "Default Course";
+        this.cID = "c00";
+        this.credits= 0;
+    }
+
+    public Course(String courseName, String courseID, int credits) {
+        super();
+        this.cName = courseName;
+        this.cID = courseID;
+        this.credits= credits;
+        sList = new ArrayList<Student>();
+    }
+
+    public String getCName() {
+        return cName;
+    }
+
+    public String getCID() {
+        return cID;
+    }
+
+    public ArrayList<Student> getsList() {
+        return sList;
+    }
+
+    public boolean enroll (Student student) {
+        if (sList.contains(student)) {
+            return false;
+        }
+        sList.add(student);
+        student.getCourseList().add(this);
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "Course [courseName=" + cName + ", courseID=" + cID + ", credits=" + credits+ "]";
+    }
 }
--- src/main/StudentEnrolment/Student.java
+++ src/main/StudentEnrolment/Student.java
@@ -1,4 +1,45 @@
 package StudentEnrolment;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String sName;
+    private String sID;
+    private String birthdate;
+    private ArrayList<Course> cList;
+
+    public Student(){
+        this.sName = "Student";
+        this.sID = "s00";
+        this.birthdate = "01/01/2021";
+    }
+
+    public Student(String studentName, String studentID, String birthdate) {
+        super();
+        this.sName = studentName;
+        this.sID = studentID;
+        this.birthdate = birthdate;
+        cList = new ArrayList<Course>();
+    }
+
+    public ArrayList<Course> getCourseList() {
+        return cList;
+    }
+
+    public String getStudentName() {
+        return sName;
+    }
+
+    public String getStudentID() {
+        return sID;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [studentName=" + sName + ", studentID=" + sID + ", birthdate=" + birthdate + "]";
+    }
 }
