91be243c29b32a36c7cf0ca4531fab823fe4db03
myusername <s3818475@rmit.edu.vn>
2021-04-06 00:24:12 +0700

    Third commit

--- src/main/StudentEnrolment/StudentEnrolment.java
+++ src/main/StudentEnrolment/StudentEnrolment.java
@@ -1,4 +1,11 @@
 package StudentEnrolment;
 
 public class StudentEnrolment {
+    public static void main(String args[]) {
+        Course c1 = new Course("c001", "SADI", 12);
+        Student s1 = new Student("s001", "Minh", "1/1/2020");
+
+        System.out.println(c1);
+        System.out.println(s1);
+    }
 }
---
+++ src/main/StudentEnrolment/StudentEnrolmentManager.java
@@ -0,0 +1,9 @@
+package StudentEnrolment;
+
+public interface StudentEnrolmentManager {
+    public void add();
+    public void delete();
+    public void update();
+    public void getOne();
+    public void getAll();
+}
